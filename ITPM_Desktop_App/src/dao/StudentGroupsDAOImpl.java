79109ade7e973ff14fab5ffa31274c0fdbe1619e
h_mandakini <himasha.mandakini@gmail.com>
2021-04-07 15:20:34 +0530

    Home Page and changes in interfaces

---
+++ ITPM_Desktop_App/src/dao/StudentGroupsDAOImpl.java
@@ -0,0 +1,122 @@
+package dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import dbConnection.DBConnection;
+import models.Student_Group;
+
+public class StudentGroupsDAOImpl {
+	
+	private DBConnection dbConnect = new DBConnection();
+
+public void insertStudentGroup(String yearAndSem, String programme, int groupNo, int subGroupNo, String groupID, String subGroupID) {
+		
+		try {
+			Connection connection = dbConnect.connect();
+			
+//			String insertQuery = "insert into testing values ('11','"+yearAndSem+"', '"+programme+"', '"+groupNo+"', '"+subGroupNo+"'"
+//					+ ", '"+groupID+"', '"+subGroupID+"') ";
+			
+			String insertQuery = "insert into student_group values (?,?,?,?,?,?,?) ";
+			
+			PreparedStatement ps = connection.prepareStatement(insertQuery);
+			ps.setInt(1, 0);
+			ps.setString(2, yearAndSem);
+			ps.setString(3, programme);
+			ps.setInt(4, groupNo);
+			ps.setInt(5, subGroupNo);
+			ps.setString(6, groupID);
+			ps.setString(7, subGroupID);
+			ps.execute();
+			
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Student_Group> listStudentGroups(){
+		
+		ArrayList<Student_Group> group_list = new ArrayList<Student_Group>();
+		
+		try {
+			Connection connection = dbConnect.connect();
+			
+			String listGroupsQuery = "select * from student_group";
+			
+			Statement statement = connection.createStatement();
+			ResultSet rs =  statement.executeQuery(listGroupsQuery);
+			while(rs.next()) {
+				Student_Group sg = new Student_Group();
+				sg.setId(rs.getString(1));
+				sg.setYearAndSemester(rs.getString(2));
+				sg.setProgramme(rs.getString(3));
+				sg.setGroupNO(rs.getInt(4));
+				sg.setSubGroupNo(rs.getInt(5));
+				sg.setGroupID(rs.getString(6));
+				sg.setSubGroupID(rs.getString(7));
+				group_list.add(sg);
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return group_list;
+	}
+	
+	public void updateStudentGroup(int id, String yearAndSem, String programme, int groupNo, int subGroupNo, String GroupID, String subGroupID) {
+		try {
+			Connection connection = dbConnect.connect();
+			String updateQuery = "update student_group set year_and_semester = '"+yearAndSem+"', programme = '"+programme+"', group_no = '"+groupNo+"',"
+					+ " sub_group_no = '"+subGroupNo+"', group_id = '"+GroupID+"', sub_group_id = '"+subGroupID+"' where student_group_id = '"+id+"' ";
+			
+			PreparedStatement ps;
+			ps = connection.prepareStatement(updateQuery);
+			ps.execute();
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteStudentGroup(int id) {
+		
+		try {
+			Connection connection = dbConnect.connect();
+			
+			String deleteQuery = "delete from student_group where student_group_id = '"+id+"'";
+			
+			PreparedStatement ps = connection.prepareStatement(deleteQuery);
+			ps.execute();
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public boolean manageGroups(String subGroupID) {
+		boolean exist = false;
+		try {
+			Connection connection = dbConnect.connect();
+			
+			String getIDs = "select student_group_id from student_group where sub_group_id = '"+subGroupID+"'";
+			PreparedStatement ps = connection.prepareStatement(getIDs);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				exist = true;
+			}else {
+				exist = false;
+			}
+			connection.close();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return exist;
+	}
+	
+}
--- ITPM_Desktop_App/src/models/Tags.java
+++ ITPM_Desktop_App/src/models/Tags.java
@@ -1,14 +1,6 @@
 package models;
 
-import java.sql.*;
-
-import java.util.ArrayList;
-
-import dbConnection.DBConnection;
-
 public class Tags {
-
-	private DBConnection dbConnection;
 	
 	private int id;
 	private String tagName;
@@ -42,86 +34,5 @@ public class Tags {
 	public void setTagCode(String tagCode) {
 		this.tagCode = tagCode;
 	}
-
-
-	public void insertTags(String tagName, String tagCode) {
-		
-		try {
-			Connection connection = dbConnection.connect();
-			
-			String insertQuery = "insert into tag values (?,?,?)";
-					
-			PreparedStatement ps = connection.prepareStatement(insertQuery);
-			ps.setInt(1, 0);
-			ps.setString(2, tagName);
-			ps.setString(3, tagCode);
-			ps.execute();
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public ArrayList<Tags> listTags(){
-			
-			ArrayList<Tags> tags_list = new ArrayList<Tags>();
-			
-			try {
-				Connection connection = dbConnection.connect();
-				
-				String listTagsQuery = "select * from tag";
-				
-				Statement statement = connection.createStatement();
-				ResultSet rs =  statement.executeQuery(listTagsQuery);
-				while(rs.next()) {
-					Tags tag = new Tags();
-					tag.setId(rs.getInt(1));
-					tag.setTagName(rs.getString(2));
-					tag.setTagCode(rs.getString(3));
-					tags_list.add(tag);
-				}
-				
-			}catch(SQLException e) {
-				e.printStackTrace();
-			}
-			return tags_list;
-		}
-	
-	
-	public void updateTags(int id , String tagName, String tagCode) {
-		
-		try {
-			
-			Connection connection = dbConnection.connect();
-			
-			String updateQuery = "update tag set tag_name = '"+tagName+"', tag_code = '"+tagCode+"' where tag_id = '"+id+"' ";
-			PreparedStatement ps = connection.prepareStatement(updateQuery);
-			ps.execute();
-			connection.close();
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public void deleteTags(int id) {
-		
-		try {
-			Connection connection = dbConnection.connect();
-			String deleteQuery = "delete from tag where tag_id = '"+id+"' ";
-			
-			PreparedStatement ps = connection.prepareStatement(deleteQuery);
-			ps.execute();
-			connection.close();
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	
 	
 }
